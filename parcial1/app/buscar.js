ec3230bddbbbfeb4fb18f62066adf26998aac7fa
Diego Garza Avalos <garzadiegoyeyo@outlook.com>
2026-02-28 11:03:48 -0600

    Navegacion entre componente con barra de navegacion

---
+++ parcial1/app/buscar.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Buscar() {
+  return (
+    
+    <View style={{ flex:1, justifyContent:'center', alignItems:'center' }}>
+      <Text>Sección Buscar</Text>
+    </View>
+  );
+}
